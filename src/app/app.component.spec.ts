9e1cd8f0c03edd5046a6533c2262621e73be8e81
Pawar <spawar3@calstatela.edu>
2023-03-26 14:20:21 -0700

    replace files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo-app'`, () => {
+  it(`should have as title 'Trial'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo-app');
+    expect(app.title).toEqual('Trial');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demo-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Trial app is running!');
   });
 });
